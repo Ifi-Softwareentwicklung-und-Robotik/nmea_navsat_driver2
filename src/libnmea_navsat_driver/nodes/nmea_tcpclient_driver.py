e3964906f74e2703dc6fd0d07155c85cb217a2a0
Norman Seyffer <norman.seyffer@informatik.tu-freiberg.de>
2023-11-24 13:40:08 +0000

    Use hostname instead ip

--- src/libnmea_navsat_driver/nodes/nmea_tcpclient_driver.py
+++ src/libnmea_navsat_driver/nodes/nmea_tcpclient_driver.py
@@ -24,7 +24,7 @@ def main(args=None):
     driver = Ros2NMEADriver()
 
     try:
-        gnss_ip = driver.declare_parameter('ip', '192.168.131.22').value
+        gnss_host = driver.declare_parameter('host', '192.168.131.22').value
         gnss_port = driver.declare_parameter('port', 9001).value
         buffer_size = driver.declare_parameter('buffer_size', 4096).value
     except KeyError as e:
@@ -33,17 +33,14 @@ def main(args=None):
 
     frame_id = driver.get_frame_id()
 
-    driver.get_logger().info("Using gnss sensor with ip {} and port {}".format(gnss_ip, gnss_port))
+    driver.get_logger().info("Using gnss sensor with ip {} and port {}".format(gnss_host, gnss_port))
 
     # Connection-loop: connect and keep receiving. If receiving fails, reconnect
     # Connect to the gnss sensor using tcp
     while rclpy.ok():
         try:
-            # Create a socket
-            gnss_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            # Connect to the gnss sensor
-            gnss_socket.connect((gnss_ip, gnss_port))
+            # Create socket and connect to the gnss sensor
+            gnss_socket = socket.create_connection((gnss_host, gnss_port))
         except socket.error as exc:
             driver.get_logger().error("Caught exception socket.error when setting up socket: %s" % exc)
             sys.exit(1)
